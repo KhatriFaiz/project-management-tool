1916c0a74f9759263b9353143dd25ce565b2d42d
Faiz Khatri <faizkhatri00@gmail.com>
2024-10-07 16:32:59 +0530

    initialize socket.io server

---
+++ server/src/index.js
@@ -0,0 +1,29 @@
+// Import required modules
+const express = require("express");
+const http = require("http");
+const socketIO = require("socket.io");
+
+// Our express app instance
+const app = express();
+
+// Create a new HTTP server
+const server = http.createServer(app);
+
+// Integrate socket.io
+const io = socketIO(server);
+
+// Listen for new connections
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  // Listen for disconnections
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+  });
+});
+
+// Start the server
+const PORT = process.env.PORT || 3001;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
